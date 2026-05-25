24687282cd403c99a59f54c54cabfefea9eddf17
assiauu <mezassia23@gmail.com>
2026-05-26 00:39:18 +0200

    premier commit : noeud camera detection couleur

---
+++ src/detection_objets/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'detection_objets'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='assia',
+    maintainer_email='assia@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+           # NODE_NAME = PACKAGE.FILE:main
+           'camera_node = detection_objets.node_camera:main', 
+        ],
+    },
+)
